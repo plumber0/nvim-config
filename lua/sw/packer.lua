952144c40b5244fd73397612dfefd54e0a4c4f20
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-05-21 22:43:40 +0900

    add lsp with Mason

--- lua/sw/packer.lua
+++ lua/sw/packer.lua
@@ -40,6 +40,28 @@ return require('packer').startup(function(use)
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
   use('nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'})
+  use('mbbill/undotree')
   use('tpope/vim-fugitive')
+
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
 
 end)
