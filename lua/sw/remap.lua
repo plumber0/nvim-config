4c5c2e17e47b80b6ccc56ed235a12341c9e11702
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-05-23 09:50:01 +0900

    edit from Vex to Ex

--- lua/sw/remap.lua
+++ lua/sw/remap.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Vex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-j>", vim.cmd.cnext)
 vim.keymap.set("n", "<C-k>", vim.cmd.cprev)
