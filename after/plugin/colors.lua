2e06c3223f1af4c43afba3884a0e4c3d9b3b0c14
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-05-23 09:46:21 +0900

    change color scheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,14 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+	color = color or "dark"
+
+	vim.cmd('set termguicolors') -- Enable true colors support
+	vim.g.ayucolor = color -- For dark version of theme
+	vim.cmd('colorscheme ayu')
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 end
 
-
 ColorMyPencils()
+
