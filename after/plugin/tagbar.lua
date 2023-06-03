c6749ba380d19d473a4f7fe0635743c013277b9e
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-06-03 17:03:44 +0900

    change TagbarToggle remap

--- after/plugin/tagbar.lua
+++ after/plugin/tagbar.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap('n', '<M-7>', ':TagbarToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>7', ':TagbarToggle<CR>', {noremap = true, silent = true})
